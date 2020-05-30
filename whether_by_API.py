c8b4e7b12a718e74ab703c6695b1717b99765f60
Said Remiev <59606092+SaidRem@users.noreply.github.com>
2020-05-30 22:06:07 +0400

    Add files via upload

--- whether_by_API.py
+++ whether_by_API.py
@@ -4,6 +4,24 @@ import requests
 from dateutil.parser import parse
 
 
+API_KEY_YAN_WEATHER = ''
+API_KEY_YAN_GEO = ''
+
+
+class YandexWeather:
+
+    def get(self, city):
+        url = f"https://api.weather.yandex.ru/v1/forecast?lat={geo[0]}&lon" \
+            f"={geo[1]}&extra=true"
+        headers = {'X-Yandex-API-Key': API_KEY_YAN_WEATHER}
+        data = requests.get(url, headers=headers).json()
+        forecast = {
+            'temp': data['fact']['temp'],
+            'pressure': data['fact']['pressure_mm']
+        }
+        return forecast
+
+
 class CityInfo:
     def __init__(self, city):
         self.city = city
@@ -12,7 +30,7 @@ class CityInfo:
         pass
 
 
-def _main(city):
+def main(city):
     city_info = CityInfo("Moscow")
     forecast = city_info.weather_forecast()
     pprint.pprint(forecast)
@@ -20,4 +38,4 @@ def _main(city):
 
 if __name__ == "__main__":
     city = input('Enter the city').strip()
-    _main(city)
+    main(city)
