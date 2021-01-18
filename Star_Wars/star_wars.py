daad6cd5fbcfd5684ff08f564dd990b10e4338a2
SaidRem <said.r@yandex.ru>
2021-01-18 09:11:09 +0400

    Added functions

--- Star_Wars/star_wars.py
+++ Star_Wars/star_wars.py
@@ -1,19 +1,25 @@
 import requests
 
-# Some urls
-url_people = 'https://www.swapi.tech/api/people/'
-url_planets = 'https://swapi.dev/api/planets/'
+# URL
+URL_PEOPLE = 'https://www.swapi.tech/api/people/'
+URL_PLANETS = 'https://swapi.dev/api/planets/'
 
 # Let's get info about first four character
+def get_people(name=None):
+    if name:
+        people = requests.get(URL_PEOPLE + name).json()
+    else:
+        people = requests.get(URL_PEOPLE).json()
+    return people
 
-all_people = requests.get(url_people).json()
 
-# Let's get info about Luke Skywalker
-
-luke_sky = requests.get(url_people + '1/').json()
 
 # Let's get info about some planets in Star Wars Universe
+def planets():
+    planets = requests.get(URL_PLANETS).json()
+    return planets
 
-planets = requests.get(url_planets).json()
 
-
+if __name__ == '__main__':
+    print(get_people('1/')) # Info about Luke Skywalker.
+    print(get_people())     # First page of characters list.
