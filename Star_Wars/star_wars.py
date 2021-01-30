228d7ab84005a1f9962b40060bd17f6e98cf0a2d
SaidRem <said.r@yandex.ru>
2021-01-30 19:09:03 +0400

    Added root func

--- Star_Wars/star_wars.py
+++ Star_Wars/star_wars.py
@@ -1,17 +1,33 @@
 import requests
 import os
+from json.decoder import JSONDecodeError
+
 # URLs
+URL_ROOT = 'https://swapi.dev/api/'
 URL_PEOPLE = 'https://www.swapi.tech/api/people/'
 URL_PLANETS = 'https://swapi.dev/api/planets/'
 URL_STARSHIPS = 'https://swapi.dev/api/planets/'
 
-def get_people(name=None):
+def root():
+    '''
+    Provides information on all available resources within the API.
+    '''
+    try:
+        result = requests.get(URL_ROOT).json()
+    except (JSONDecodeError, IndexError, KeyError):
+        pass
+    return result
+
+def get_people(name=None, url=None):
     '''
     Returns dict object with characters of the Star Wars
     if not specified name of a character.
        '''
+    # params = dict(page=2, limit=10) # notation for getting pages
     if name:
         people = requests.get(URL_PEOPLE + name).json()
+    elif url:
+        people = requests.get(url).json()
     else:
         people = requests.get(URL_PEOPLE).json()
     return people
@@ -38,6 +54,9 @@ def starships(name=None):
     return starships
 
 if __name__ == '__main__':
-    print(get_people('1/')) # Info about Luke Skywalker.
-    print(get_people())     # First page of characters list.
-    print(starships())
+    # print(get_people('1/')) # Info about Luke Skywalker.
+    # print(get_people())        # First page of characters list.
+    print(root())
+    # nex = get_people()['next']
+    # print(get_people(url=nex))
+    # print(starships())
