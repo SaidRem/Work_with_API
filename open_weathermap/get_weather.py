e2ab1808f8627b34eed17e4664d660abe8242c3a
SaidRem <said.r@yandex.ru>
2021-02-01 17:22:41 +0400

    Added func to get current weather

--- open_weathermap/get_weather.py
+++ open_weathermap/get_weather.py
@@ -1,8 +1,28 @@
 import requests
 
-API_KEY = ""
-URL_API = ""
+
+with open("api_key.txt") as f:
+    API_KEY = f.readline().rstrip()
 
 
-def get_weather():
-    pass
+URL_API = r"http://api.openweathermap.org/data/2.5"
+
+
+def today_weather(city=None, msr="metric"):
+    """Returns current weather in a city"""
+    if city:
+        p = dict(q=city, units=msr, appid=API_KEY)
+    else:
+        p = dict(q="Astrakhan", units=msr, appid=API_KEY)
+    r = requests.get(f"{URL_API}//weather", params=p)
+    r = r.json()
+    return r
+
+
+if __name__ == "__main__":
+    city = input("Enter city name => ")
+    if city:
+        print(today_weather(city=city))
+    else:
+        print(today_weather())
+
