a13aa34f3a4ac14dafc2331ea8ce1b953c3e00e3
SaidRem <said.r@yandex.ru>
2021-02-07 18:25:37 +0400

    Added function: coordinates()

--- Geolocation/geolocation.py
+++ Geolocation/geolocation.py
@@ -6,6 +6,11 @@ def geoloc():
     r = requests.get("http://freegeoip.app/json/").json()
     return r
 
+def coordinates():
+    r = geoloc()
+    return dict(latitude=r["latitude"], longitude=r["longitude"])
+
 
 if __name__ == "__main__":
     pprint(geoloc())
+    print(f'Your coordinates:\n{coordinates()}')
