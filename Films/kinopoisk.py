92af8376e93c7f01c6b42ca78504a3f24c8fab43
SaidRem <said.r@yandex.ru>
2021-02-25 17:05:17 +0400

    Added movie func and url

--- Films/kinopoisk.py
+++ Films/kinopoisk.py
@@ -1,6 +1,15 @@
 import requests
+from pprint import pprint
 
-URL_API_KINO = ''
+with open('api_token.txt') as f:
+    API_TOKEN = f.readline().strip()
 
-def kino():
-    pass
+URL_API_KINO = 'https://api.kinopoisk.cloud'
+
+def movie(id):
+    res = requests.get(URL_API_KINO + f'/movies/{id}' + f'/token/{API_TOKEN}')
+    return res.json()
+
+
+if __name__ == '__main__':
+    pprint(movie(1143242))
