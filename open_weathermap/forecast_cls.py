c5c67e1255e6d12d02f41413ba785201f98d96fa
SaidRem <said.r@yandex.ru>
2021-04-15 22:25:18 +0400

    Added __str__ method

--- open_weathermap/forecast_cls.py
+++ open_weathermap/forecast_cls.py
@@ -10,9 +10,19 @@ URL_API = r"http://api.openweathermap.org/data/2.5"
 
 
 class OpenW:
-    def __init__(self, city, msr="metric"):
-        self.p = dict(q=city, units=msr, appid=API_KEY)
-        res = requests.get(f"{URL_API}//forecast", params=p)
+    def __init__(self, city=None, msr="metric"):
+        city = city if city else "Moscow"
+        self.params = dict(q=city, units=msr, appid=API_KEY)
+        res = requests.get(f"{URL_API}//forecast", params=self.params)
         if res.status_code != 200:
             print(f"Not Found the city: {city}")
-        self.5dforecast = res.json()
+        self.five_dforecast = res.json()
+    
+    def __str__(self):
+        pprint(self.five_dforecast['city'])
+
+
+if __name__ == '__main__':
+    city = input('Enter city\n=> ')
+    ob = OpenW(city)
+    print(ob)
