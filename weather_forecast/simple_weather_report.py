30b7eb6d9b54af0e67a014d5bdff2a32fd91daf5
SaidRem <said.r@yandex.ru>
2021-06-12 23:40:10 +0400

    Added options

--- weather_forecast/simple_weather_report.py
+++ weather_forecast/simple_weather_report.py
@@ -1,6 +1,13 @@
 import sys
 import requests
 
-resp = requests.get(f"https://wttr.in/{' '.join(sys.argv[1:])}")
+def weather(city):
+    resp = requests.get(f"https://wttr.in/city")
+    print(resp.text)
 
-print(resp.text)
+
+if __name__ == '__main__':
+    if len(sys.argv) > 1:
+        weather(' '.join(sys.argv[1:]))
+    else:
+        weather(input('Enter your city => '))
