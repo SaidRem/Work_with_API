cac81105ff9b8492c7a23180b0c0bbc3514f9abf
SaidRem <said.r@yandex.ru>
2021-08-23 21:20:35 +0400

    Added

---
+++ twitter_api/hidden.py
@@ -0,0 +1,8 @@
+# https://apps.twitter.com/
+# Create new App and get the four strings
+
+def oauth():
+    return {"consumer_key": "",
+            "consume_secret": "",
+            "token_key": "",
+            "token_secret": ""}
---
+++ twitter_api/twtest.py
@@ -0,0 +1,24 @@
+import urllib.request, urllib.parse, urllib.error
+from twurl import augment
+import ssl
+
+# https://apps.twitter.com/
+
+SCREEN_NAME = ''
+COUNT = '2'
+
+print('* Calling Twitter...')
+url = augment('https://api.twitter.com/1.1/statuses/user_timeline.json',
+              {'screen_name': SCREEN_NAME, 'count': COUNT})
+
+# Ignore SSL certificate errors
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
+
+connection = urllib.request.urlopen(url, context=ctx)
+data = connection.read()
+print(data)
+
+headers = dict(connection.getheaders())
+# print(headers)
